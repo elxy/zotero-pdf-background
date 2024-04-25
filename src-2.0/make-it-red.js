5ba20d76f1dd2e0b3e5427de71a60955140fd7d0
elxy <elxy@outlook.com>
2024-04-26 00:50:29 +0800

    feat: use menu item to toggle style

--- src-2.0/make-it-red.js
+++ src-2.0/make-it-red.js
@@ -5,6 +5,7 @@ MakeItRed = {
 	initialized: false,
 	addedElementIDs: [],
 	notifierID: null,
+	styleID: 'pdf-background',
 
 	init({id, version, rootURI}) {
 		if (this.initialized) return;
@@ -18,41 +19,88 @@ MakeItRed = {
 		Zotero.debug("Make It Red: " + msg);
 	},
 
-	stylingReader(reader) {
-		this.log(reader._title);
+	getStyleHTMLFromReader(reader) {
 		let viewer = reader._iframeWindow.wrappedJSObject.PDFViewerApplication.pdfViewer.viewer;
-		var elements = viewer.getElementsByClassName('textLayer');
-		// 遍历这些元素并设置样式
-		for (var i = 0; i < elements.length; i++) {
-			elements[i].style.display = 'block';
-			elements[i].style.backgroundColor = 'rgba(155,189,133,0.5)';
+		// 获取父元素的所有子元素
+		var children = viewer.childNodes;
+		// 遍历子元素
+		var finded = [];
+		for (var i = 0; i < children.length; i++) {
+			var child = children[i];
+			// 检查子元素的ID
+			if (child.id === this.styleID) {
+				finded.push(child);
+			}
+		}
+		return finded;
+	},
+
+	stylingReader(reader, enabled) {
+		let styleChildren = this.getStyleHTMLFromReader(reader);
+
+		if (enabled && styleChildren.length) {
+			this.log(reader.tabID + " is already styled, no need to add style HTML");
+			return;
+		}
+		if (!enabled && !styleChildren.length) {
+			this.log(reader.tabID + " is not styled, no need to remove style HTML");
+			return;
+		}
+
+		let viewer = reader._iframeWindow.wrappedJSObject.PDFViewerApplication.pdfViewer.viewer;
+		if (enabled) {
+			this.log(reader.tabID + " is not styled, adding style HTML");
+			let color = Zotero.Prefs.get('pdf-background.style.background-color', true);
+			let css = '.textLayer { display: block; background-color: ' + color + '; }';
+			let styleHTML = '<style type="text/css" id=' + this.styleID + '>' + css + '</style>';
+			viewer.insertAdjacentHTML('beforeend', styleHTML)
+		} else {
+			this.log(reader.tabID + " is styled, removing style HTML");
+			for (let child of styleChildren) {
+				viewer.removeChild(child);
+			}
+		}
+	},
+
+	removeStyleHTMLFromReader(reader) {
+		this.log("Remove style HTML from " + reader.tabID);
+
+		if (!this.addedReaderIDs.get(reader.tabID)) {
+			this.log("No need to remove style HTML");
+			return;
+		}
+
+		let viewer = reader._iframeWindow.wrappedJSObject.PDFViewerApplication.pdfViewer.viewer;
+		// 获取父元素的所有子元素
+		var children = viewer.childNodes;
+		// 遍历子元素
+		for (var i = 0; i < children.length; i++) {
+			var child = children[i];
+			// 检查子元素的ID
+			if (child.id === this.styleID) {
+				viewer.removeChild(child);
+			}
 		}
 	},
 
 	addToWindow(window) {
 		let doc = window.document;
 
-		// Add a stylesheet to the main Zotero pane
-		let link1 = doc.createElement('link');
-		link1.id = 'make-it-red-stylesheet';
-		link1.type = 'text/css';
-		link1.rel = 'stylesheet';
-		link1.href = this.rootURI + 'style.css';
-		doc.documentElement.appendChild(link1);
-
-		this.storeAddedElement(link1);
-
 		// Use Fluent for localization
 		window.MozXULElement.insertFTLIfNeeded("make-it-red.ftl");
 
 		// Add menu option
-		let menuitem = doc.createXULElement('menuitem');
-		menuitem.id = 'make-it-green-instead';
+		var menuitem = doc.createXULElement('menuitem');
+		menuitem.id = 'pdf-background-enabled';
 		menuitem.setAttribute('type', 'checkbox');
-		menuitem.setAttribute('data-l10n-id', 'make-it-red-green-instead');
+		menuitem.setAttribute('data-l10n-id', 'pdf-background-enabled');
+		let enabled = Zotero.Prefs.get('pdf-background.style.enabled', true);
+		menuitem.setAttribute('checked', enabled ? 'true' : 'false');
 		// MozMenuItem#checked is available in Zotero 7
 		menuitem.addEventListener('command', () => {
-			MakeItRed.toggleGreen(window, menuitem.checked);
+			let checked = menuitem.getAttribute('checked');
+			this.log('checked? ' + checked);
+			MakeItRed.toggleStyling(window, checked);
 		});
 		doc.getElementById('menu_viewPopup').appendChild(menuitem);
 		this.storeAddedElement(menuitem);
@@ -88,11 +136,18 @@ MakeItRed = {
 			if (!win.ZoteroPane) continue;
 			this.removeFromWindow(win);
 		}
+
+		for (let reader of Zotero.Reader._readers) {
+			this.removeStyleHTMLFromReader(reader);
+		}
 	},
 
-	toggleGreen(window, enabled) {
-		window.document.documentElement
-			.toggleAttribute('data-green-instead', enabled);
+	toggleStyling(window, enabled) {
+		this.log('toggleStyling ' + enabled);
+		Zotero.Prefs.set('pdf-background.style.enabled', enabled, true);
+		for (let reader of Zotero.Reader._readers) {
+			this.stylingReader(reader, enabled);
+		}
 	},
 
 	registerNotifier() {
@@ -100,7 +155,9 @@ MakeItRed = {
 			notify: async (event, type, ids, extraData) => {
 				if (event == "select" && type == "tab" && extraData[ids[0]].type == "reader") {
 					var reader = Zotero.Reader.getByTabID(ids[0]);
-					this.stylingReader(reader);
+					var enabled = Zotero.Prefs.get('pdf-background.style.enabled', true);
+					this.log('notify ' + enabled);
+					this.stylingReader(reader, enabled);
 				}
 			},
 		};
@@ -114,11 +171,8 @@ MakeItRed = {
 	},
 
 	async main() {
-		// Global properties are included automatically in Zotero 7
-		var host = new URL('https://foo.com/path').host;
-		this.log(`Host is ${host}`);
-
 		// Retrieve a global pref
-		this.log(`Intensity is ${Zotero.Prefs.get('extensions.make-it-red.intensity', true)}`);
+		this.log(`enabled is ${Zotero.Prefs.get('pdf-background.style.enabled', true)}`);
+		this.log(`Background color is ${Zotero.Prefs.get('pdf-background.style.background-color', true)}`);
 	},
 };
--- src-2.0/prefs.js
+++ src-2.0/prefs.js
@@ -1,1 +1,2 @@
-pref("extensions.make-it-red.intensity", 100);
+pref("pdf-background.style.background-color", "rgba(155,189,133,0.8)");
+pref("pdf-background.style.enabled", true);
